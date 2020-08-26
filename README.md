d8cb78c4e4b08d16162105965140baefcf337557
jamesdborin <james.dborin@gmail.com>
2020-08-26 17:09:02 +0100

    Initial plotting code, Exercise network and exampled exercises included

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 Workout Tracker App
+
+Description:
+-----
+This is an app designed for simple and quick storage, access, and visualisation
+of data generated as part of a workout routine.
+
+This is differentiated from most apps by a unique relational graph which defines
+a hierarchy of exercises from specific movements/speeds/strengh curves to large
+movement patterns. This allows the user to see volume accumulated over multiple 
+hierarchy levels. This is meant to accommodate the issue with usual tracking 
+software which penalises variety in exercsie choice as either: 
+
+a) switching between exercises causes gaps in work even if you are doing 
+closely related exercises
+
+ 
+
--- TrackerApp/ExerciseNetwork.py
+++ TrackerApp/ExerciseNetwork.py
@@ -3,6 +3,7 @@ import networkx as nx
 import json
 from networkx.drawing.nx_agraph import graphviz_layout
 from networkx.readwrite.json_graph import node_link_data, node_link_graph
+from collections import deque 
 
 class ExerciseNetwork:    
     """
@@ -12,7 +13,7 @@ class ExerciseNetwork:
         - *NetworkX DiGraph*: pass initial_graph = <nx.DiGraph> object
         - *JSON file*: pass a dictionary loaded from a JSON file
         - *Nothing*: to initialise a new graph
-        - *file_to_graph*: run this method with a filename to load a graph from a file
+        - *file_to_graph*: run this method with a filename to load a graph f ExerciseNEtworkrom a file
     
     
     Useful Methods:
@@ -20,11 +21,26 @@ class ExerciseNetwork:
     
     - add_branch / add_branches / add_branches_from_dict
     - get_children_of / get_parents_of
-    - add_edge_from_name / add_edgess_from_dict
+    - add_edge_from_name / add_edges_from_dict
     - draw_graph
     - get_exercise_names
     """
+    
     def __init__(self, initial_graph = None, json_graph = None):
+        """
+        Initialise an exercise network, either from a network X object or with
+        a graph defined as a dictionary
+        
+        *initial_graph*: Given a DiGraph, load this as an exercise network
+        
+        *json_graph*: Given a dictionary defining a graph, load this as the 
+        Exercise Network
+        
+        --------------
+        N.B. To load from a file you do the following.
+        E.g. Ex = ExerciseNetwork() 
+        Ex.file_to_graph(filename)
+        """
         is_graph = isinstance(initial_graph, nx.DiGraph)
         is_dict = isinstance(json_graph, dict)
         if is_graph:
@@ -37,11 +53,17 @@ class ExerciseNetwork:
         
     @property
     def all_labels(self):
+        """
+        Return the names of every node in the graph
+        """
         return [node[1]['name'] for node in self.graph.nodes(data=True)]
     
     
     @staticmethod
     def get_common_elements(list1, list2):
+        """
+        Given two lists return the common elements as a set
+        """
         set1, set2 = set(list1), set(list2)
         common_elements = set1 & set2
         return common_elements
@@ -49,14 +71,36 @@ class ExerciseNetwork:
     
     @staticmethod
     def get_unique_elements(list1, list2):
+        """
+        Given two lists return any elements that are unique to either set
+        """
         set1, set2 = set(list1), set(list2)
         unique_elements = set1 ^ set2
         return unique_elements
     
     
     def add_branch(self, new_labels, start_node_name):
-        # check if any of the new labels are currently in the graph. No new node is added and a new edge is added.
+        """
+        Add a branch to the Exercise Network:
+            0. Specify the Parent node name and any child node that the parent 
+                is to connect to
+            
+            1. Identify nodes in new branches that already exist in the 
+                Network
+                
+            2. Add any nodes that dont already exist in the graph to the 
+                graph
+                
+            3. Add edges from the parent nodes to the new nodes
+            
+            4. Any nodes that already exist must have an edge pointing towards
+                them added to the network
+                
+        """
+        # check if any of the new labels are currently in the graph. 
+        #   No new node is added and a new edge is added.
         common_labels = self.get_common_elements(self.all_labels, new_labels)
+        
         # Isolate new labels that are unique
         unique_labels = self.get_unique_elements(common_labels, new_labels)
         
@@ -80,6 +124,12 @@ class ExerciseNetwork:
     
     
     def add_branches(self, new_labels, start_node_names):
+        """
+        For a list of start nodes add new labels to those nodes.
+        
+        *New Labels*: [[A1, A2, A3, ...], [B1, B2, ...], ...]
+        *Start_Node_Labels*: [A_New, B_New, ...]
+        """
         for label, start in zip(new_labels, start_node_names):        
             self.add_branch(label, start)
        
@@ -95,23 +145,70 @@ class ExerciseNetwork:
     
     
     def get_labels_of_nodes(self, nodes):
+        """
+        Input: A list of node numbers
+        
+        Output: The names of exercises on those nodes
+        """
         return [y['name'] for x,y in self.graph.nodes(data = True) 
                 if x in nodes]
     
     
     def get_nodes_of_labels(self, labels):
-        return [x for x,y in self.graph.nodes(data = True) if y['name'] 
-                in labels]
+        """
+        Input: List of Exercise Names
+        
+        Output: List of node numbers that belong to those exercises
+        """
+        return [x for x,y in filter(lambda x: x[1]['name'] in labels, self.graph.nodes(data = True))]
      
     
     def get_children_of(self, name):
+        """
+        Given a node label find the children of that node.
+        
+        Input: Label of the node you want the children of
+        
+        Ouput: [Child Node Numbers, Child Node Names]
+        """
         start_node = self.get_nodes_of_labels(name)[0]
         child_nodes = list(self.graph.successors(start_node))
         child_names = self.get_labels_of_nodes(child_nodes)
         return child_nodes, child_names
     
-    
+    def get_descendants_of(self, name):
+        """
+        Given a node label, find all end nodes that descend from this node
+
+        Input: Label of the node you want the children of
+
+        Output: [Descendant Node Numbers, Descendant Node Names]
+        """
+        end_nodes = []
+        start_node = self.get_nodes_of_labels(name)[0]
+        q = deque()
+        q.append(start_node)
+        
+        while len(q) > 0:
+            node = q.popleft()
+            node_label = self.get_labels_of_nodes([node])
+            children = self.get_children_of(node_label)
+            
+            for child in children[0]:
+                successors_exist = ( len(list(self.graph.successors(child))) > 0 )
+                if successors_exist:
+                    q.appendleft(child)
+
+                else:
+                    end_nodes.append(child)
+        
+        return end_nodes
+
+        
     def get_parents_of(self, name):
+        """
+        Given a node label find the parents of that node
+        """
         start_node = self.get_nodes_of_labels(name)[0]
         parent_nodes = list(self.graph.predecessors(start_node))
         parent_names = self.get_labels_of_nodes(parent_nodes)
@@ -119,6 +216,9 @@ class ExerciseNetwork:
 
     
     def add_edge_from_name(self, start_label, end_label):
+        """
+        Add an edge between two node names
+        """
         nodes = self.get_nodes_of_labels([start_label, end_label])
         edges = [(nodes[0], nodes[1])]
         self.graph.add_edges_from(edges)
@@ -132,12 +232,29 @@ class ExerciseNetwork:
             for end_label in edges_dict[start_label]:
                 self.add_edge_from_name(start_label, end_label)
 
-    
+    def delete_branch(self, delete_name):
+        """
+        We want to delete an entire branch from the network if necessary.
+        
+        Recursively delete a branch and its child nodes. Pray to the gods it
+        doesnt delete something you want to keep.
+        """
+        node_num = self.get_nodes_of_labels([delete_name])
+        child_nodes = self.get_children_of([delete_name])
+        
+        if len(child_nodes[0]) > 0:
+            for node in child_nodes[1]:
+                self.delete_branch(node)
+            
+        self.graph.remove_node(node_num[0])
+        
+
     def draw_graph(self, size = (20,20)):
         pos = graphviz_layout(self.graph, prog='twopi', args = '')
         labels = {x:y['name'] for x,y in self.graph.nodes(data = True)}
         plt.figure(figsize = size)
-        nx.draw(self.graph, pos, node_size=0, alpha=0.4, edge_color="r", font_size=8, with_labels=True, labels = labels)
+        nx.draw(self.graph, pos, node_size=0, alpha=0.4, edge_color="r", 
+                font_size=8, with_labels=True, labels = labels)
     
     
     def get_exercise_names(self):
@@ -164,3 +281,11 @@ class ExerciseNetwork:
         with open(filename) as json_file:
             data = json.load(json_file)
             self.graph = node_link_graph(data)
+            
+            
+if __name__ == "__main__":
+    EX = ExerciseNetwork()
+    EX.file_to_graph("./stored_info/first_layer_exercise_chart.txt")
+    EX.add_branches_from_dict({"Bicep Curls":["Hammer Curls"], "Grip Strength": ["Grip Curls"], "Grip Curls": ["Hammer Curls"]})
+    EX.graph_to_file("./stored_info/first_layer_exercise_chart.txt")
+    EX.draw_graph()
